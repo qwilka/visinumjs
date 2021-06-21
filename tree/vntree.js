e02343bc3d4e32c508159729b71f84ad126c4257
Stephen McEntee <stephenmce@gmail.com>
2021-06-21 23:06:01 +0100

    vntree index.html

--- tree/vntree.js
+++ tree/vntree.js
@@ -1,7 +1,7 @@
 
 // node -i -e "$(< vntree.js)"
 
-class Node {
+class VnNode {
     constructor(name, parent=null, data={}) {
         this.name = name;
         this.parent = parent;
@@ -44,19 +44,32 @@ class Node {
 }
 
 
+let run_tests = false;
+if (run_tests || require.main===module) {
+    console.log("Launching tests for ", __filename)
+    runTests()
+}
 
+function runTests() {
+    let rootnode = new VnNode("root node");
+    let node1 = new VnNode("node1 level1", rootnode);
+    let node11 = new VnNode("node11 level2", node1);
+    let node12 = new VnNode("node12 level2", node1);
+    let node13 = new VnNode("node13 level2", node1);
+    let node121 = new VnNode("node121 level3", node12);
+    let node122 = new VnNode("node122 level3", node12);
+    let node2 = new VnNode("node2 (level1)", rootnode);
+    let node21 = new VnNode("node21 (level2)", node2);
+    let node211 = new VnNode("node211 (level3)", node21);
+    let node212 = new VnNode("node212 (level3) parentless");
+    node21.add_child(node212);
+    node21.add_child(new VnNode("node213 (level3) parentless"));
+    let node3 = new VnNode("node3 (level1)", rootnode);
+    
+    console.log(rootnode.toTextTree());
+    for (let n of rootnode) console.log(n.name);
+}
 
-let rootnode = new Node("root node");
-let node1 = new Node("node1 level1", rootnode);
-let node11 = new Node("node11 level2", node1);
-let node12 = new Node("node12 level2", node1);
-let node13 = new Node("node13 level2", node1);
-let node121 = new Node("node121 level3", node12);
-let node122 = new Node("node122 level3", node12);
-let node2 = new Node("node2 (level1)", rootnode);
-let node21 = new Node("node21 (level2)", node2);
-let node211 = new Node("node211 (level3)", node21);
-let node212 = new Node("node212 (level3) parentless");
-node21.add_child(node212);
-node21.add_child(new Node("node213 (level3) parentless"));
-let node3 = new Node("node3 (level1)", rootnode);
+module.exports = VnNode;
+//export {VnNode};
+
